b5317c73e25c7babb41a2e62d142d00c73f082ed
meitalrevah <103144149+meitalrevah@users.noreply.github.com>
2024-09-11 16:13:08 +0300

    Add files via upload

---
+++ Wpf_Markups/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Markups
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                btn.Background = Brushes.Red;
+            }
+
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                btn.Background = new SolidColorBrush(Colors.Blue);
+
+                e.Handled = true; //גורם לזה לצבוע רק את הריבוע הפנימי בלי החיצוני
+            }
+        }
+    }
+}
---
+++ Wpf_Markups/TimeExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Wpf_Markups
+{
+    internal class TimeExtension : MarkupExtension
+    {
+        public string Format { get; set; }
+
+        public TimeExtension()
+        {
+            
+        }
+
+        public TimeExtension(string format)
+        {
+            Format = format;
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return DateTime.Now.ToString(Format);
+        }
+    }
+}
